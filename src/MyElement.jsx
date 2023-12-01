a168a191a2a50319e04dc26e373d164ef6d0634a
alexchun <alexchun78@gmail.com>
2023-12-01 16:18:54 +0900

    leva 추가, args를 통한 제어

--- src/MyElement.jsx
+++ src/MyElement.jsx
@@ -1,9 +1,55 @@
-import React,{useRef} from 'react'
+import React,{useRef,useEffect} from 'react'
 import { OrbitControls, Box } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three';
+import {useControls} from 'leva';
 
 function MyElement3D() {
+  const refMesh = useRef();  
+  const refMeshWireFrame = useRef();
+
+  const {xSize,ySize,zSize,xDivide,yDivide,zDivide} = useControls ({
+    xSize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01
+    },
+    ySize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01
+    },
+    zSize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01
+    },
+    xDivide: {
+      value: 1,
+      min: 1,
+      max: 8,
+      step: 1
+    },
+    yDivide: {
+      value: 1,
+      min: 1,
+      max: 8,
+      step: 1
+    },
+    zDivide: {
+      value: 1,
+      min: 1,
+      max: 8,
+      step: 1
+    },
+  });
+  useEffect(()=>{
+    refMeshWireFrame.current.geometry = refMesh.current.geometry;
+  },[xSize,ySize,zSize,xDivide,yDivide,zDivide]);
+  
   return (
     <>      
       <ambientLight intensity={0.1}/>
@@ -11,13 +57,12 @@ function MyElement3D() {
       <axesHelper scale={10}/>
       <OrbitControls/>
       
-      <mesh>
-        <boxGeometry />          
+      <mesh ref = {refMesh}> 
+        <boxGeometry args={[xSize,ySize,zSize,xDivide,yDivide,zDivide]}/>          
         <meshStandardMaterial color="#e67e22" opacity={0.7} transparent={true}/>
       </mesh>
 
-      <mesh>
-        <boxGeometry />          
+      <mesh ref = {refMeshWireFrame}>
         <meshStandardMaterial emissive={"yellow"} wireframe={true}/>
       </mesh>
     </>
